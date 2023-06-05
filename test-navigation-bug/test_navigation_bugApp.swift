ea6642f5c291943740eb5378910d753fd59fbcfe
David Arvelo <david@davidarvelo.com>
2023-06-05 16:05:08 +0700

    Initial Commit

---
+++ test-navigation-bug/test_navigation_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_navigation_bugApp.swift
+//  test-navigation-bug
+//
+//  Created by David Arvelo on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct test_navigation_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
